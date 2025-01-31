d235e04df2faf3c8c69b22b11160f198a1e5e7ba
Whereisade <eniolaadeleke2303@gmail.com>
2025-01-31 12:21:44 +0100

    FEAT: Fixed time

--- scripts.js
+++
@@ -1,7 +0,0 @@
-function updateTime() {
-    const utcTimeElement = document.getElementById("utc-time");
-    const now = new Date();
-    utcTimeElement.textContent = `Current UTC Time: ${now.toISOString().split("T")[1].split(".")[0]}`;
-}
-
-updateTime();
